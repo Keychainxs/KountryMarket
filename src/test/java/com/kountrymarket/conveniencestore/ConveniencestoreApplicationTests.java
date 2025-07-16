8ff3d87235021f166823f4584c2367c55620b4ff
Keychainxs <151799816+Keychainxs@users.noreply.github.com>
2025-07-16 11:34:32 -0400

    Adding Files

---
+++ src/test/java/com/kountrymarket/conveniencestore/ConveniencestoreApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kountrymarket.conveniencestore;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConveniencestoreApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
